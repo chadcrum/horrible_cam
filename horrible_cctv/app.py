c67c04abd2ee3e1ddbdb89a385c0fd8c15dd1b04
root <root@camtop.boob.support>
2018-06-10 19:37:26 -0500

    added cctv viewing list of files

--- horrible_cctv/app.py
+++ horrible_cctv/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from datetime import datetime, timedelta
+import humanfriendly
+import requests
 import glob
 import json
 import re
@@ -22,13 +25,61 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    bla = 'test'
-    file_dir = '/recordings/driveway'
-    files = os.listdir(file_dir)
-    files = glob.glob(file_dir + "/*.mp4")
-    files.sort(key=os.path.getmtime, reverse=True)
-    logger.debug(files)
-    return bla
+    base_url = os.environ['BASE_URL']
+    test_url = base_url + 'backyard_fence/'
+    bla = sort_json_files(test_url)
+    logger.debug(bla)
+    crap = '<html><head></head><body>'
+    for i in bla:
+        crap += '<video width="320" height="240" controls> <source src="' + test_url + i['name'] + '" type="video/mp4"></video><br>'
+        
+    return crap
+
+@app.route("/videos/<video_dir>", methods=['GET'])
+def list_videos(video_dir):
+    time_offset = os.environ['TIME_OFFSET']
+    video_url = os.environ['BASE_URL'] + video_dir + '/'
+    bla = sort_json_files(video_url)
+    logger.debug(bla)
+    crap = '<html><head></head><body>'
+    for i in bla:
+        tmp_date = i['mtime_dt'] - timedelta(hours=int(os.environ['TIME_OFFSET']))
+        crap += '<h3>' + tmp_date.strftime('%b %d, %Y - %H:%M:%S') + ' - ' + str(humanfriendly.format_size(i['size'])) + '</h3>'
+        crap += '<video width="320" height="240" controls preload="none"> <source src="' + video_url + i['name'] + '" type="video/mp4"></video><br>'
+        crap += "<hr>"
+    return crap
+    
+
+def get_file_list(url):
+    """Retrieves a json file list from a url"""
+    session = requests.Session()
+    session.trust_env = False
+    try:
+        r = session.get(url, timeout=30, headers={'Content-type': 'application/json', 'Accept': 'application/json'})
+        if r.status_code == 200:
+            logger.debug('Retrieved remote file list return state code %s', r.status_code)
+            file_list = json.loads(r.text)
+            return file_list
+        else:
+            connection_error = "Error retrieving remote file list with status code %s and message %s and url %s" % (r.status_code, r.text, url)
+            raise ConnectionError(connection_error)
+    except ConnectionError:
+        logger.exception('Problem connecting to remote server %s to retrieve file list', url)
+        raise
+    return file_list
+
+def sort_json_files(url):
+    file_list = []
+    # Get raw file list
+    raw_file_list = get_file_list(url)
+    for i in raw_file_list:
+        if i['type'] == 'file':
+            # Adds mtime_dt datetime field - "mtime":"Sun, 29 Oct 2017 15:10:00 GMT"
+            i['mtime_dt'] = datetime.strptime(i['mtime'].replace(' GMT', ''), '%a, %d %b %Y %H:%M:%S')
+            file_list.append(i)
+    # Sort list of dict by [{'mtime_dt':] desc
+    file_list = sorted(file_list, key = lambda k: k["mtime_dt"], reverse=True)
+    return file_list
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
